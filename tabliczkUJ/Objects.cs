a3ce7feebe5c2da3aa1559a9aaa929c3b8853f9c
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2024-01-21 21:52:16 +0100

    @tjzel/json (#1)
    * add json
    
    * stuff

---
+++ tabliczkUJ/Objects.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System.Windows.Controls;
+
+namespace tabliczkUJ
+{
+  interface IContentObject
+  {
+    public string Serialize();
+    public void SetPosition(int x, int y);
+    public void SetSize(int x, int y);
+  }
+
+  class DoorTag
+  {
+    public string Name { get; set; }
+    public ICollection<IContentObject> ContentObjects;
+  }
+
+  class LogoObject : IContentObject
+  {
+    public Image Image { get; set; }
+
+    public string Serialize()
+    {
+      return JsonConvert.SerializeObject(this);
+    }
+
+    public void SetPosition(int x, int y)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void SetSize(int x, int y)
+    {
+      throw new NotImplementedException();
+    }
+  }
+
+  class RoomNumberObject : IContentObject
+  {
+    public string RoomNumberText { get; set; }
+
+    public string Serialize()
+    {
+      return JsonConvert.SerializeObject(this);
+    }
+
+    public void SetPosition(int x, int y)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void SetSize(int x, int y)
+    {
+      throw new NotImplementedException();
+    }
+  }
+
+  class RoomMembersObject : IContentObject
+  {
+    public string RoomMembersText { get; set; }
+
+    public string Serialize()
+    {
+      return JsonConvert.SerializeObject(this);
+    }
+
+    public void SetPosition(int x, int y)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void SetSize(int x, int y)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
