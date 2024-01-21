d223f0f99632fca05276ccecfa92af0bfff45082
eporada <e.porada8@gmail.com>
2024-01-21 16:49:51 +0100

    upload logo button

--- tabliczkUJ/MainWindow.xaml.cs
+++ tabliczkUJ/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,5 +21,18 @@ namespace tabliczkUJ
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+            {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Uri fileUri = new Uri(openFileDialog.FileName);
+                logoImage.Source = new BitmapImage(fileUri);
+            }
+        }
     }
+
+    
 }
